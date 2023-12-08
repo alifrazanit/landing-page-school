409999929358488b6501076d67cf7a5b9c6bdd97
Alif Razan Saputra <alifrazanit@gmail.com>
2023-12-09 00:44:19 +0700

    sambutan kepala sekolah

--- src/app/components/portal-web/banner/banner.component.ts
+++ src/app/components/portal-web/banner/banner.component.ts
@@ -1,11 +1,9 @@
 import { Component, OnInit, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-import { register } from 'swiper/element/bundle';
-
-register();
+import {MatButtonModule} from '@angular/material/button';
 @Component({
   selector: 'app-banner',
   standalone: true,
-  imports: [],
+  imports: [MatButtonModule],
   templateUrl: './banner.component.html',
   styleUrl: './banner.component.css',
   schemas:[CUSTOM_ELEMENTS_SCHEMA]
--- src/app/pages/portal-web/portal-web.component.ts
+++ src/app/pages/portal-web/portal-web.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { HeaderComponent  } from '@components/portal-web/header/header.component';
 import { HeaderMenuComponent  } from '@components/portal-web/header-menu/header-menu.component';
 import { BannerComponent } from '@components/portal-web/banner/banner.component';
+import { AboutUsComponent } from '@components/portal-web/about-us/about-us.component';
 
 
 @Component({
@@ -10,7 +11,8 @@ import { BannerComponent } from '@components/portal-web/banner/banner.component'
   imports: [
     HeaderComponent,
     HeaderMenuComponent,
-    BannerComponent],
+    BannerComponent,
+    AboutUsComponent],
   templateUrl: './portal-web.component.html',
   styleUrl: './portal-web.component.css'
 })
