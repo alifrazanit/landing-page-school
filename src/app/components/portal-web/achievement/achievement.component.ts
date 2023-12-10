52fcbd11a32d0699cc71b46e2558903823d0e2b6
Alif Razan Saputra <alifrazanit@gmail.com>
2023-12-10 17:37:01 +0700

    update achievement

--- src/app/components/portal-web/achievement/achievement.component.ts
+++ src/app/components/portal-web/achievement/achievement.component.ts
@@ -1,12 +1,30 @@
 import { Component } from '@angular/core';
+import { MatGridListModule } from '@angular/material/grid-list';
 
 @Component({
   selector: 'app-achievement',
   standalone: true,
-  imports: [],
+  imports: [MatGridListModule],
   templateUrl: './achievement.component.html',
   styleUrl: './achievement.component.css'
 })
 export class AchievementComponent {
-
+  images: any[] = [
+    {
+      image: 'assets/images/achievement-1.jpg',
+      cols: 3, rows: 1
+    },
+    {
+      image: 'assets/images/achievement-2.jpg',
+      cols: 1, rows: 2,
+    },
+    {
+      image: 'assets/images/achievement-4.jpg',
+      cols: 1, rows: 1,
+    },
+    {
+      image: 'assets/images/achievement-3.jpg',
+      cols: 2, rows: 1,
+    },
+  ]
 }
